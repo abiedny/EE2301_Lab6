20a9191fc9d1b398894b431da6d81e41afe6c7d2
Alex Biedny <abiedny09@gmail.com>
2018-12-06 14:25:56 -0600

    FINALLY fixed the issue preventing synthesis and implementation

--- Lab6.srcs/sources_1/new/Multiplier.v
+++ Lab6.srcs/sources_1/new/Multiplier.v
@@ -1,68 +1,32 @@
 `timescale 1ns / 1ps    
 
-//this works somehow. It's just combinational and sorta for testing I guess?
-module Multiplier(x, y, out, done);
-    input [7:0] x;
-    input [7:0] y;
-    output [15:0] out;
-    output done;
-    reg [15:0] out;
-    reg done;
-    
-    initial begin
-        done = 0;
-        out = 16'b0;
-            if (y[7] == 1) begin
-                out = out + (x << 7);  
-            end
-            if (y[6] == 1) begin
-                out = out + (x << 6);  
-            end
-            if (y[5] == 1) begin
-                out = out + (x << 5);  
-            end
-            if (y[4] == 1) begin
-                out = out + (x << 4);  
-            end
-            if (y[3] == 1) begin
-                out = out + (x << 3);
-            end
-            if (y[2] == 1) begin
-                out = out + (x << 2);  
-            end
-            if (y[1] == 1) begin
-                out = out + (x << 1);  
-            end
-            if (y[0] == 1) begin
-                out = out + (x << 0);  
-            end   
-        done = 1;                                                                                 
-    end
-endmodule
-
-module Wrapper(x, y, start, state, clock, out);
-    input [7:0] x;
-    input [7:0] y;
-    input start; //button u press to start it
-    output state;
+module Wrapper(sw, btnC, clock, led);
+    input [15:0] sw;
+    reg [7:0] x;
+    reg [7:0] y;
+    input btnC; //button u press to start it
     input clock;
-    output [15:0] out;
-    reg state;
+    output [15:0] led;
+    reg state = 2'b00;
     reg [15:0] out;
     
-    initial begin
-        state = 'b0;
-    end
+    assign led = out;
     
-    always @(start) begin
+    /*always @(btnC) begin
         //when the start button is pressed our inputs are ready and we can go to state 1 to do that math
         //or if we're in the final state then go back to the start state
-        if (state == 'b0) state = 'b1;
-        else if (state == 'b10) state = 'b0;
-    end
+        //we also gonna load the x ad y values on the switches into memory just cuz
+        if (state == 2'b00) begin
+            x = sw[7:0];
+            y = sw[15:8];
+            #50
+            state = 2'b01;
+        end
+        else if (state == 2'b10) state = 2'b00;
+    end*/
     
     always @(posedge clock) begin
-        if (state == 'b1) begin //if we're in the multiplication state, do multiplication
+        if (state == 2'b01) begin //if we're in the multiplication state, do multiplication
             if (y[7] == 1) begin
                 out = out + (x << 7);  
             end
@@ -88,10 +52,26 @@ module Wrapper(x, y, start, state, clock, out);
                 out = out + (x << 0);  
             end
             //and then when we done multiplying we move states to state 2
-            state = 'b10; 
+            state = 2'b10; 
         end
-        else if (state == 'b10) begin
+        else if (state == 2'b10) begin
             //Multiplication is done so display it
+            //ok it should be displaying because of procedural assignment, so I guess we do nothing here?
+        end
+        else if (state == 2'b00) begin
+            //clear out the out register once we're back at the begining
+            //this will clear the LED's as well
+            out[15:0] = 'b0;
+        end
+        
+        if (btnC) begin
+            if (state == 2'b00) begin
+                x = sw[7:0];
+                y = sw[15:8];
+                #50
+                state = 2'b01;
+            end
+            else if (state == 2'b10) state = 2'b00;
         end
     end
     
